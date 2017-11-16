1af78014a3d4ee6d432cee7f747bd5f3240f4017
Keith Mok <kmok@cyngn.com>
2017-11-15 22:53:50 -0500

    base: SystemUI: Add Caffeine QS Tile
    Change-Id: Ic83aa6f800a1b8d036a46336fc37fed66e034846

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -29,6 +29,7 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
@@ -182,6 +183,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new RebootTile(mHost);
             case "usb_tether":
                 return new UsbTetherTile(mHost);
+            case "caffeine":
+                return new CaffeineTile(mHost);
         }
 
         // Intent tiles.
