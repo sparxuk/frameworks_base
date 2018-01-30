7242b291a895f9db87e665545fb8803479d6adc7
xyyx <xyyx@mail.ru>
2018-01-30 14:35:41 +0800

    base: Add toggle for face auto unlock (1/2)
    Change-Id: Ic0158c92b5c924c8d741b2d47f705027bba1b134

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9195,6 +9195,16 @@ public final class Settings {
         private static final Validator AWARE_LOCK_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Enable face auto unlock on secure lock screens
+         *
+         * @hide
+         */
+        public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
+        /** @hide */
+        private static final Validator FACE_AUTO_UNLOCK_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
         * Boolean value whether to link ringtone and notification volume
         * @hide
         */
@@ -9346,7 +9356,8 @@ public final class Settings {
             SILENCE_TIMER_TOUCH_COUNT,
             DARK_MODE_DIALOG_SEEN,
             GLOBAL_ACTIONS_PANEL_ENABLED,
-            AWARE_LOCK_ENABLED
+            AWARE_LOCK_ENABLED,
+            FACE_AUTO_UNLOCK
         };
 
         /**
@@ -9542,6 +9553,7 @@ public final class Settings {
             VALIDATORS.put(GLOBAL_ACTIONS_PANEL_ENABLED, GLOBAL_ACTIONS_PANEL_ENABLED_VALIDATOR);
             VALIDATORS.put(AWARE_LOCK_ENABLED, AWARE_LOCK_ENABLED_VALIDATOR);
             VALIDATORS.put(VOLUME_LINK_NOTIFICATION, VOLUME_LINK_NOTIFICATION_VALIDATOR);
+            VALIDATORS.put(FACE_AUTO_UNLOCK, FACE_AUTO_UNLOCK_VALIDATOR);
         }
 
         /**
