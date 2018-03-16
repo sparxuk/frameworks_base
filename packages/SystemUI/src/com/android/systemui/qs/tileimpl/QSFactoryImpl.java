c1cf190358ca1e87988ca65791daa9fbbd674326
SuperDroidBond <superdroidbond@yahoo.com>
2018-03-16 16:53:28 +0000

    Add ScreenStabilizationTile in QS
    Forward Port from Nougat -> Oreo -> Pie -> Android 10 by me
    
    Disable Long click for now
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -47,6 +47,7 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
+import com.android.systemui.qs.tiles.ScreenStabilizationTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -215,6 +216,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mScreenRecordTileProvider.get();
             case "sync":
                 return mSyncTileProvider.get();
+            case "screenstabilization":
+                return new ScreenStabilizationTile(mHost);
         }
 
         // Intent tiles.
