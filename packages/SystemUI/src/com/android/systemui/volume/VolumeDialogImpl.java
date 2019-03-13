6361ce5eeba938c594c0943f54bb00b86d2b7995
Giuseppe Maggio <jertlok@protonmail.com>
2019-03-13 22:31:57 +0100

    Code improvements for expandable volume panel
    - Do not remove and add rows multiple times, this may cause
      slow downs. Just make the already existing rows visible or
      not according to the active streams and expandable logic;
    - Do not separate the expanded view from the little one, this
      makes the dialog dismiss in a consistent way (as one block);
    - Ensure that both the extra rows (STREAM_RING & STREAM_ALARM)
      get shown if they are required: with the previous logic we
      could potentially remove these rows when needed. With the
      visibility trick we are just safe;
    - Removed methods that are no longer used.
    
    Change-Id: I23792369cf1fd8e0b4d6913c67d02bd4c09f1ac7

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -353,12 +353,6 @@ public class VolumeDialogImpl implements VolumeDialog {
         }
     }
 
-    private void updateAllActiveRows() {
-        int N = mRows.size();
-        for (int i = 0; i < N; i++)
-            updateVolumeRowH(mRows.get(i));
-    }
-
     private VolumeRow getActiveRow() {
         for (VolumeRow row : mRows) {
             if (row.stream == mActiveStream) {
@@ -457,9 +451,10 @@ public class VolumeDialogImpl implements VolumeDialog {
     private void cleanExpandRows() {
         for(int i = mRows.size() - 1; i >= 0; i--) {
             final VolumeRow row = mRows.get(i);
-            if (row.stream == AudioManager.STREAM_RING ||
-                    row.stream == AudioManager.STREAM_ALARM)
-                removeRow(row);
+            if ((row.stream == AudioManager.STREAM_RING ||
+                    row.stream == AudioManager.STREAM_ALARM) && row.stream != mActiveStream) {
+                Util.setVisOrGone(row.view, /* isVisible */ false);
+            }
         }
     }
 
@@ -476,11 +471,18 @@ public class VolumeDialogImpl implements VolumeDialog {
         });
         mExpandRows.setOnClickListener(v -> {
             if(!mExpanded) {
-                addRow(AudioManager.STREAM_RING, R.drawable.ic_volume_ringer,
-                        R.drawable.ic_volume_ringer_mute, true, false);
-                addRow(AudioManager.STREAM_ALARM,
-                        R.drawable.ic_volume_alarm, R.drawable.ic_volume_alarm_mute, true, false);
-                updateAllActiveRows();
+                VolumeRow row = findRow(AudioManager.STREAM_RING);
+                if (row != null) {
+                    Util.setVisOrGone(row.view, true);
+                    updateVolumeRowTintH(row,
+                            /* isActive */ row.stream == mActiveStream);
+                }
+                row = findRow(AudioManager.STREAM_ALARM);
+                if (row != null) {
+                    Util.setVisOrGone(row.view, true);
+                    updateVolumeRowTintH(row,
+                            /* isActive */ row.stream == mActiveStream);
+                }
                 mExpanded = true;
             } else {
                 cleanExpandRows();
@@ -636,6 +638,8 @@ public class VolumeDialogImpl implements VolumeDialog {
                 .withEndAction(() -> mHandler.postDelayed(() -> {
                     if (D.BUG) Log.d(TAG, "mDialog.dismiss()");
                     mDialog.dismiss();
+                    mExpanded = false;
+                    mExpandRows.setExpanded(mExpanded);
                 }, 50));
         if (!isLandscape()) animator.translationX((mDialogView.getWidth() / 2)*(!isAudioPanelOnLeftSide() ? 1 : -1));
         animator.start();
@@ -648,10 +652,6 @@ public class VolumeDialogImpl implements VolumeDialog {
                 mSafetyWarning.dismiss();
             }
         }
-
-        cleanExpandRows();
-        mExpanded = false;
-        mExpandRows.setExpanded(mExpanded);
     }
 
     private boolean shouldBeVisibleH(VolumeRow row, VolumeRow activeRow) {
@@ -978,7 +978,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         boolean useActiveColoring = isActive && row.slider.isEnabled();
         final ColorStateList tint = useActiveColoring ? mActiveTint : mInactiveTint;
         final int alpha = useActiveColoring ? mActiveAlpha : mInactiveAlpha;
-        if (tint == row.cachedTint) return;
+        if (tint == row.cachedTint && mExpanded) return;
         row.slider.setProgressTintList(tint);
         row.slider.setThumbTintList(tint);
         row.slider.setProgressBackgroundTintList(tint);
