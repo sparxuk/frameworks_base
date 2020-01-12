de22c2754a311661f41de91f4cce0327c795748c
fraz14 <fraz140@gmail.com>
2020-01-12 14:09:07 +0100

    DeviceUtils: add deviceSupportsFlashLight

--- core/java/com/android/internal/util/gzosp/DeviceUtils.java
+++ core/java/com/android/internal/util/gzosp/DeviceUtils.java
@@ -21,6 +21,9 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.WifiDisplayStatus;
 import android.hardware.SensorManager;
@@ -149,6 +152,28 @@ public class DeviceUtils {
     public static class FilteredDeviceFeaturesArray {
         public String[] entries;
         public String[] values;
+    }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
     }
 
     private static int getScreenType(Context con) {
